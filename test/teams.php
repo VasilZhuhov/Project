ba2ad7f523b9ac5fb1781ad397ef00536220a7a9
VasilZhuhov <vaset0_98@abv.bg>
2016-04-10 11:48:57 +0300

    Rabote6ti promeni

--- test/teams.php
+++ test/teams.php
@@ -23,8 +23,11 @@ print_r($dif);*/
 
 
 $query = "INSERT INTO `matches` (`id`, `team_1_id`, `team_2_id`) VALUES (NULL, 1, 2)";
-
 $newMatch = $db->fetchArray($query);
+
+$query = "SELECT id FROM matches ORDER BY id DESC LIMIT 1";
+$match_id = $db->fetchArray($query);
+$match_id = $match_id[0]["id"];
 
 $ballPos = ($team1[0][2]-$team2[0][2] + 50);
 //echo "$ballPos <br>";
@@ -44,41 +47,46 @@ $goalTeam2 = 0;
 
 $min = 0;
 
+$data = ["id"=> NULL, "match_id"=> $match_id, "minute"=> 0, "action_id"=> 0, "team_id"=> 0];
+
+
 while($min <= 89)
 {
 	$min++;
+	$data["minute"] = $min;
     if( rand(0,100) < 36)
     {
+    	$data["team_id"] = 1;
 		if( rand(0,100) <= $ballPos )
 		{
 			if( rand(0,100) < 70 + $chanceCreated)
 			{
 				if( rand(0,100) < 30 - $blockedShotTeam1)
 				{
-					$query = 'INSERT INTO `minutes` (`id`, `match_id`, `minute`, `action_id`, `team_id`) VALUES (NULL, 1, 1, 5, 1)';  
-					$newMin = $db->fetchArray($query);
+					$data["action_id"] = 5;
+					$newMin = $db->saveArray("minutes", $data);
 					echo "Blocked shot";
 				}
 				else
 				{
 					if( rand(0,100) < 30 + $missedShotTeam1)
 					{
-						$query = 'INSERT INTO `minutes` (`id`, `match_id`, `minute`, `action_id`, `team_id`) VALUES (NULL, 1, 1, 3, 1)';  
-						$newMin = $db->fetchArray($query);
+						$data["action_id"] = 3;
+						$newMin = $db->saveArray("minutes", $data);
 						echo "Miss";
 					}
 					else
 					{
 						if( rand( 0,100) < 60 + $team1GK)
 						{
-							$query = 'INSERT INTO `minutes` (`id`, `match_id`, `minute`, `action_id`, `team_id`) VALUES (NULL, 1, 1, 4, 1)';  
-							$newMin = $db->fetchArray($query);
+							$data["action_id"] = 4;
+							$newMin = $db->saveArray("minutes", $data);
 							echo "Save";
 						}
 						else
 						{
-							$query = 'INSERT INTO `minutes` (`id`, `match_id`, `minute`, `action_id`, `team_id`) VALUES (NULL, 1, 1, 2, 1)';  
-							$newMin = $db->fetchArray($query);
+							$data["action_id"] = 2;
+							$newMin = $db->saveArray("minutes", $data);
 							echo "Goal";
 							$goalTeam1++;
 
@@ -88,42 +96,42 @@ while($min <= 89)
 			}
 			else
 			{
-
-				$query = 'INSERT INTO `minutes` (`id`, `match_id`, `minute`, `action_id`, `team_id`) VALUES (NULL, 1, 1, 1, 1)';  
-				$newMin = $db->fetchArray($query);
+				$data["action_id"] = 1;
+				$newMin = $db->saveArray("minutes", $data);
 			}
 		  
 		}
 		else
 		{
+			$data["team_id"] = 2;
 			if( rand(0,100) < 70 + $chanceCreated)
 			{
 					if( rand(0,100) < 30 - $blockedShotTeam2)
 					{
-						$query = 'INSERT INTO `minutes` (`id`, `match_id`, `minute`, `action_id`, `team_id`) VALUES (NULL, 1, 1, 5, 2)';  
-						$newMin = $db->fetchArray($query);
+						$data["action_id"] = 5;
+						$newMin = $db->saveArray("minutes", $data);
 						echo "Blocked shot";
 					}
 					else
 					{
 						if( rand(0,100) < 30 + $missedShotTeam2)
 						{
-							$query = 'INSERT INTO `minutes` (`id`, `match_id`, `minute`, `action_id`, `team_id`) VALUES (NULL, 1, 1, 3, 2)';  
-							$newMin = $db->fetchArray($query);
+							$data["action_id"] = 3;
+							$newMin = $db->saveArray("minutes", $data);
 							echo "Miss";
 						}
 						else
 						{
 							if( rand( 0,100) < 60 + $team2GK)
 							{
-								$query = 'INSERT INTO `minutes` (`id`, `match_id`, `minute`, `action_id`, `team_id`) VALUES (NULL, 1, 1, 4, 2)';  
-								$newMin = $db->fetchArray($query);
+								$data["action_id"] = 4;
+								$newMin = $db->saveArray("minutes", $data);
 								echo "Save";
 							}
 							else
 							{
-								$query = 'INSERT INTO `minutes` (`id`, `match_id`, `minute`, `action_id`, `team_id`) VALUES (NULL, 1, 1, 2, 2)';  
-								$newMin = $db->fetchArray($query);
+								$data["action_id"] = 2;
+								$newMin = $db->saveArray("minutes", $data);
 								echo "Goal";
 								$goalTeam2++;
 							}
@@ -132,17 +140,17 @@ while($min <= 89)
 			}
 			else
 			{
-
-					$query = 'INSERT INTO `minutes` (`id`, `match_id`, `minute`, `action_id`, `team_id`) VALUES (NULL, 1, 1, 1, 2)';  
-					$newMin = $db->fetchArray($query);
+				$data["action_id"] = 1;
+				$newMin = $db->saveArray("minutes", $data);
 			}
 			
 		}
 	}
 	else
 	{
-		$query = 'INSERT INTO `minutes` (`id`, `match_id`, `minute`, `action_id`, `team_id`) VALUES (NULL, 1, 1, 6, 0)';  
-		$newMin = $db->fetchArray($query);
+		$data["action_id"] = 6;
+		$data["team_id"] = 0;
+		$newMin = $db->saveArray("minutes", $data);
 	}
     echo "<br> $min <br>";
 }
@@ -151,7 +159,7 @@ echo "$goalTeam1  <br>";
 echo "$goalTeam2  <br>";
 
 /*
-В таблицата minutes  на мястото на match_id,minute съм задал да въвежда 1-ници, трябва да въвежда съответното id на мача и минутата.Горе е на Жоро кода за съотвеното взимане на стойности от избраните отбори(не работи) има го и в 
+В таблицата minutes на мястото на match_id, minute съм задал да въвежда 1-ници, трябва да въвежда съответното id на мача и минутата. Горе е на Жоро кода за съотвеното взимане на стойности от избраните отбори(не работи) има го и в 
 1.php заедно със селекта.Засега взима стойностите от масивите.
 
 
