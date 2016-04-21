bc7eb4022510cc64bf69b54fd2899fa45d15d5e6
nakata77 <sagittarian@abv.bg>
2016-04-21 22:16:09 +0300

    ddd

--- game/aftermatch.php
+++ game/aftermatch.php
@@ -70,61 +70,55 @@ require_once('ajax.php');
         <!-- /.container -->
     </nav>
 
-	<div class="gamebg">
-        <div class="menu">
-            <img class='backg' src="../img/stadium.jpg">
-        </div>
-    </div>
- 		<div>
+    	<div class="gamebg">
             <div class='teamSelection'>
-                <div class='yourTeam'>
-                    <div class='teamImg'>
-                       	<img src="../img/<?php echo $team1[0]['id']?>.png">
-                    </div>
-                    <div class='teamShots' id='shotsT1'>
-                    	0
-                    </div>
-                    <div class='teamSaves' id='savesT1'>
-                    	0
-                    </div>
-                    <div class='teamMisses' id='missT1'>
-                    	0
-                    </div>
-                    <div class='wasted' id='wastedT1'>
-                        0
-                    </div>
-                </div> 
-                <div class='stats'>
-                	<div class='result'>
-                		<span id='teamgoal1'>0</span>
-                		-
-                		<span id='teamgoal2'>0</span>
-                	</div>
-                	<div class='min' id='min'>0</div>
-                	<div class='shots'>Shots</div>
-                	<div class='saves'>Saves</div>
-                	<div class='misses'>Misses</div>
-                    <div class='wastes'>Failed attempts</div>
-                </div>  
-                <div class='opponent'>
-                    <div class='teamImg1'>
-                      	<img src="../img/<?php echo $team2[0]['id']?>.png">
-                    </div>
-                    <div class='teamShots' id='shotsT2'>
-                    	0
-                    </div>
-                     <div class='teamSaves' id='savesT2'>
-                    	0
-                    </div>
-                    <div class='teamMisses' id='missT2'>
-                    	0
-                    </div>
-                    <div class='wasted' id='wastedT2'>
-                        0
-                    </div>
-                </div> 
+                    <div class='yourTeam'>
+                        <div class='teamImg'>
+                           	<img src="../img/<?php echo $team1[0]['id']?>.png">
+                        </div>
+                        <div class='teamShots' id='shotsT1'>
+                        	0
+                        </div>
+                        <div class='teamSaves' id='savesT1'>
+                        	0
+                        </div>
+                        <div class='teamMisses' id='missT1'>
+                        	0
+                        </div>
+                        <div class='wasted' id='wastedT1'>
+                            0
+                        </div>
+                    </div> 
+                    <div class='stats'>
+                    	<div class='result'>
+                    		<span id='teamgoal1'>0</span>
+                    		-
+                    		<span id='teamgoal2'>0</span>
+                    	</div>
+                    	<div class='min' id='min'>0</div>
+                    	<div class='shots'>Shots</div>
+                    	<div class='saves'>Saves</div>
+                    	<div class='misses'>Misses</div>
+                        <div class='wastes'>Failed attempts</div>
+                    </div>  
+                    <div class='opponent'>
+                        <div class='teamImg1'>
+                          	<img src="../img/<?php echo $team2[0]['id']?>.png">
+                        </div>
+                        <div class='teamShots' id='shotsT2'>
+                        	0
+                        </div>
+                         <div class='teamSaves' id='savesT2'>
+                        	0
+                        </div>
+                        <div class='teamMisses' id='missT2'>
+                        	0
+                        </div>
+                        <div class='wasted' id='wastedT2'>
+                            0
+                        </div>
+                    </div> 
             </div>
-        </div> 
-		</div> 
-	</body>
+    	</div> 
+    </body>
 </html>
